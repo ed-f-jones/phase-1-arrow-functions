774b8fa31c530d34ce6c27eafe9667e000b4fa14
Edward <ed.f.jones@gmail.com>
2023-04-09 16:35:41 -0400

    lab complete

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = () => {
+    let answer = 2000 / 100
+    return answer
+}
+
+const square = (number) => {
+ let sum = 2 * number
+ return sum
+}
+
+const add = (num1, num2) => {
+    let sum = num1 + num2
+    return sum
+}
